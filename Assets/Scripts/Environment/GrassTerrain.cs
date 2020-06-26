5df77940fbd41a97bf59d9ddcb0d304a6881af7d
TomBurch <thomas.burch1306@gmail.com>
2020-06-26 14:42:07 +0100

    Removed irrelevant assets / code

--- Assets/Scripts/Environment/GrassTerrain.cs
+++ Assets/Scripts/Environment/GrassTerrain.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 //using Cube;
 
 namespace Terrain {
+
     public class GrassTerrain : MonoBehaviour { 
-        //public Transform grassPrefab;
-        //public Vector3[,] cubePositions;
 
         public TerrainData Generate(int size)
         {
@@ -19,26 +18,19 @@ namespace Terrain {
                 for (int x = 0; x < size; x++)
                 {
                     terrainData.terrainCubes[z, x] = new Cubes.GrassCube(z, x, floorObject, string.Format("{0}-{1}-{2}", x, 1, z));
+                }
+            }
 
-                //Vector3 tilePosition = new Vector3((x * 1f) + 0.5f, 0.0f, (z * 1f) + 0.5f);
-                //Transform grassTile = Instantiate(grassPrefab, tilePosition, Quaternion.identity, floorObject.transform);
-                //grassTile.name = string.Format("{0}-{1}-{2}", x, 1, z);
-
-                //terrainData.cubePositions[z, x] = tilePosition;
-        }
-    }
             return terrainData;
         }
 
         public class TerrainData {
             public int size;
             public Cubes.TerrainCube[,] terrainCubes;
-            //public Vector3[,] cubePositions;
 
             public TerrainData(int size) {
                 this.size = size;
                 terrainCubes = new Cubes.TerrainCube[size, size];
-                //cubePositions = new Vector3[size, size];
             }
         }
     }
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Terrain;
+
+public class Mover : MonoBehaviour
+{
+    Environment environment;
+    float lastMovementTime;
+
+    void Start()
+    {
+        environment = FindObjectOfType<Environment>();
+        lastMovementTime = Time.time;
+    }
+
+    void Update()
+    {
+        float currentTime = Time.time;
+        if (currentTime - lastMovementTime >= 1)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(0f, 10f), 0.5f, Random.Range(0f, 10f));
+            move(randomPos);
+            lastMovementTime = Time.time;
+        }
+    }
+
+    public bool move(Vector3 newGridPos)
+    {
+        Cubes.TerrainCube[,] terrainCubes = environment.getTerrainData().terrainCubes;
+        transform.position = terrainCubes[(int)newGridPos.x, (int)newGridPos.z].getPos() + new Vector3(0f, 0.5f, 0f);
+
+        return true;
+    }
+}
